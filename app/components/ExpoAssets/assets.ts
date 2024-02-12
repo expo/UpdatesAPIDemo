07feaac8ee8013af8b5f2841179faf6efbbcb9f9
Douglas Lowder <douglowder@mac.com>
2024-02-12 11:37:45 -0800

    Add default assets for demo build

--- app/components/ExpoAssets/assets.ts
+++ app/components/ExpoAssets/assets.ts
@@ -7,5 +7,13 @@ export type ImageAsset = {
 }
 
 export const assets: ImageAsset[] = [
-
+{
+    name: "red_blue.jpg",
+    directory: "embeddedImages",
+    image: require("../../../app/embeddedImages/red_blue.jpg")
+  },{
+    name: "red_green.jpg",
+    directory: "embeddedImages",
+    image: require("../../../app/embeddedImages/red_green.jpg")
+  }
 ]
