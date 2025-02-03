a410a46e403edf6389b930e127266c68b214a0ba
Douglas Lowder <douglowder@mac.com>
2025-02-02 20:03:44 -0800

    Remove splash screen code

--- App.tsx
+++ App.tsx
@@ -2,10 +2,11 @@ import App from "./app/app"
 import React from "react"
 import * as SplashScreen from "expo-splash-screen"
 
-SplashScreen.preventAutoHideAsync()
+// SplashScreen.preventAutoHideAsync()
 
 function IgniteApp() {
-  return <App hideSplashScreen={SplashScreen.hideAsync} />
+  // return <App hideSplashScreen={SplashScreen.hideAsync} />
+  return <App hideSplashScreen={async () => {}} />
 }
 
 export default IgniteApp
